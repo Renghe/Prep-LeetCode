436ecfe79c97dccfb289823162a96895c2bd3511
Tanvish Renghe <92147280+Renghe@users.noreply.github.com>
2025-04-26 23:16:04 +0530

    Update 33_Search_Insert_Position.py

--- Easy/33_Search_Insert_Position.py
+++ Easy/33_Search_Insert_Position.py
@@ -5,15 +5,16 @@ class Solution:
         left, right = 0, len(nums) - 1
 
         while left <= right:
-            mid = (left+right)//2
+            mid = (left+right)//2  #mid = 2,l=0,r=4           
 
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
-                left = mid + 1
+            elif nums[mid] < target: 
+                left = mid + 1 #left = 0
             else:
-                right = mid - 1
+                right = mid - 1 # right = 2-1 = 1, mid = 2
         return left
     
 sol = Solution()
-print(sol.searchInsert([1,3,4,5], 2))
+print(sol.searchInsert([1,3,8,9,10], 7))
+
