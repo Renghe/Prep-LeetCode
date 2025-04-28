5f1d17465dff4799d48639c764aae8b97d9e2c74
Tanvish Renghe <92147280+Renghe@users.noreply.github.com>
2025-04-28 20:05:30 +0530

    Create 58_Length_of_Last_word.py

---
+++ Easy/58_Length_of_Last_word.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        
+        word = s.strip().split()
+
+        return len(word[-1])
+
+sol = Solution()
+print(sol.lengthOfLastWord("Hello World"))
+
+
