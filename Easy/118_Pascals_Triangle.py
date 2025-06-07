9c5fcd0fbae33702043ea777f2642ca43859237a
Tanvish Renghe <92147280+Renghe@users.noreply.github.com>
2025-06-07 19:59:12 +0530

    Update 118_Pascals_Triangle.py

--- Easy/118_Pascals_Triangle.py
+++ Easy/118_Pascals_Triangle.py
@@ -1,13 +1,13 @@
 class Solution:
     def generate(self, numRows: int) -> list[list[int]]:
-        triangle = []
+        result = []
 
         for i in range(numRows):
             
             row = [1] * (i + 1)
             
             for j in range(1, i):
-                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-            triangle.append(row)
+                row[j] = result[i - 1][j - 1] + result[i - 1][j]
+            result.append(row)
 
-        return triangle
+        return result
