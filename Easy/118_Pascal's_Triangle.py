696ddb2b6555b6230c4a51be5f27e5b1fa8c0cac
Tanvish Renghe <92147280+Renghe@users.noreply.github.com>
2025-06-07 17:32:12 +0530

    Update 118_Pascal's_Triangle.py

--- Easy/118_Pascal's_Triangle.py
+++ Easy/118_Pascal's_Triangle.py
@@ -4,8 +4,8 @@ class Solution:
         result = []
 
         for i in range(numRows):
-            row = [1] * (i + 1)  # start with 1s
-            for j in range(1, i):  # fill inner values
+            row = [1] * (i - 1)  
+            for j in range(1, i):  
                 row[j] = result[i - 1][j - 1] + result[i - 1][j]
             result.append(row)
 
