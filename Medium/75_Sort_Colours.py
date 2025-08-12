1070953034dea339c11e42def038f41372500fe2
Tanvish Renghe <renghetanvish@gmail.com>
2025-08-12 23:05:11 +0530

    Update 75_Sort_Colours.py

--- Medium/75_Sort_Colours.py
+++ Medium/75_Sort_Colours.py
@@ -3,16 +3,17 @@ class Solution:
         low, mid, high = 0, 0, len(nums) - 1
 
         while mid <= high:
+
             if nums[mid] == 0:
                 nums[low], nums[mid] = nums[mid], nums[low]
                 low += 1
                 mid += 1
-
+            
             elif nums[mid] == 1:
                 mid += 1
             
             else:
-                nums[mid], nums[high] = nums[high], nums[mid]
+                nums[high], nums[mid] = nums[mid], nums[high]
                 high -= 1
 
 nums = [0,1,2,1,0]
